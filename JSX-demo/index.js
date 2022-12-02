11ad07136c403bd63569178fb4c24af8dd6f44ba
Wh1t3D0v3 <chris-bradwell@live.co.uk>
2022-12-02 16:33:05 +0000

    JSX-component

--- JSX-demo/index.js
+++ JSX-demo/index.js
@@ -1,1 +1,9 @@
-class JSXDemo extends React.Component {}
+class JSXDemo extends React.Component {
+  render() {
+    return (
+      <img src="https://images.unsplash.com/photo-1669908750572-112a3e982409?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80" />
+    );
+  }
+}
+
+ReactDOM.render(<JSXDemo />, document.getElementById("root"));
